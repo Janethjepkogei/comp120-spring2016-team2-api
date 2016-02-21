2ae7fe0585a2bd6f7e8219abfbc5f64850208c4d
Christopher Hinstorff <chris@chinstorff.com>
2016-02-21 22:05:51 +0000

    set up unicorn

--- app.rb
+++ app.rb
@@ -3,53 +3,57 @@ require 'sinatra/json'
 require 'sinatra/cross_origin'
 require './db'
 
-configure do
+class DirtApp < Sinatra::Base
+  register Sinatra::CrossOrigin
+
   enable :cross_origin
-end
 
-get '/' do
-  return "DIRT api"
-end
-
-post '/incidents/new' do
-  incident = Incident.create(:description => params[:description],
-                             :location => params[:location],
-                             :severity => params[:severity],
-                             :status => 0,
-                             :user_id => 1)
-
-  puts "description: '#{params[:description]}'"
-  if incident.saved?
-    return json incident
-  else
-    return "Failed to create incident"
+  get '/' do
+    cross_origin
+    return "DIRT api"
   end
-end
 
-get '/incidents/:id' do |id|
-  return json Incident.get(id)
-end
+  post '/incidents/new' do
+    incident = Incident.create(:description => params[:description],
+                               :location => params[:location],
+                               :severity => params[:severity],
+                               :status => 0,
+                               :user_id => 1)
 
-post '/incidents/:id' do |id|
-  fields = [:description, :location, :severity, :status]
-  incident = Incident.get(id)
-  fields.each do |field|
-    if params[field] and not incident.update field => params[field]
-      return json "Failed to update #{field}"
+    puts "description: '#{params[:description]}'"
+    if incident.saved?
+      return json incident
+    else
+      return "Failed to create incident"
     end
   end
-  return json incident
-end
 
-get '/incidents' do
-  fields = [
-            :id,
-#            :user,
-            :severity,
-            :description,
-#            :departments,
-            :created_at,
-            :status,
-           ]
-  return json Incident.all(:fields => fields)
+  get '/incidents/:id' do |id|
+    return json Incident.get(id)
+  end
+
+  post '/incidents/:id' do |id|
+    fields = [:description, :location, :severity, :status]
+    incident = Incident.get(id)
+    fields.each do |field|
+      if params[field] and not incident.update field => params[field]
+        return json "Failed to update #{field}"
+      end
+    end
+    return json incident
+  end
+
+  get '/incidents' do
+    fields = [
+              :id,
+              #            :user,
+              :severity,
+              :description,
+              #            :departments,
+              :created_at,
+              :status,
+             ]
+    return json Incident.all(:fields => fields)
+  end
+
 end
